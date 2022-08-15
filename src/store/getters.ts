1d64d79fe99d8172d4913488a638607f791f06e7
hyungkiShin <tkaqkeldk@nate.com>
2022-08-15 12:34:33 +0900

    [feat] getters Typing

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,27 +1,43 @@
-export default {
-  fetchedItem(state : any) {
-    return state.item;
+import { RootState } from "./state";
+
+// computed 속성에서 1:1 mapping 이 된다.
+// 사용시기: 일반적인 state 에 접근을 할때
+export const getters = {
+  // state 를 가공 연산해서 컴포넌트에서 쉽게 접근 할 수 있게 해주는 게 getter 다
+  fetchedNews(state: RootState) {
+    return state.news;
   },
-  fetchedUser(state : any) {
-    return state.user;
-  },
-  fetchedList(state : any) {
-    return state.list;
-  },
-  // ItemView
-  userName(state : any) {
-    return state.item.user;
-  },
-  userContent(state : any) {
-    return state.item.content;
-  },
-  userQuestion(state : any) {
-    return state.item.title;
-  },
-  userTimeAgo(state : any) {
-    return state.item.time_ago;
-  },
-  contentPoints(state: any) {
-    return state.item.points;
-  },
+  // getToken(state: RootState) {
+  //   return state.token + "!";
+  // }
 };
+
+export type Getters = typeof getters;
+
+// export default {
+//   fetchedItem(state : any) {
+//     return state.item;
+//   },
+//   fetchedUser(state : any) {
+//     return state.user;
+//   },
+//   fetchedList(state : any) {
+//     return state.list;
+//   },
+//   // ItemView
+//   userName(state : any) {
+//     return state.item.user;
+//   },
+//   userContent(state : any) {
+//     return state.item.content;
+//   },
+//   userQuestion(state : any) {
+//     return state.item.title;
+//   },
+//   userTimeAgo(state : any) {
+//     return state.item.time_ago;
+//   },
+//   contentPoints(state: any) {
+//     return state.item.points;
+//   },
+// };
